61bcf724a292f249c116ba544a74e29502be7ffc
Sachin Charakhwal <sachincharakhwal@gmail.com>
2022-09-09 19:41:36 +0530

    feat(maps): add keymaps to focus clients

--- keymaps/global.lua
+++ keymaps/global.lua
@@ -7,4 +7,12 @@ return {
   map({ opts.modkey }, 'Return', function()
     spawn(opts.terminal)
   end),
+
+  map({ opts.modkey }, 'j', function()
+    awful.client.focus.byidx(1)
+  end, { description = 'focus next by index', group = 'CLIENT' }),
+
+  map({ opts.modkey }, 'k', function()
+    awful.client.focus.byidx(-1)
+  end, { description = 'focus previous by index', group = 'CLIENT' }),
 }
