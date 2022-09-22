89091890937b1fe498404290dd21f7ccdebc4d58
Sachin Charakhwal <sachincharakhwal@gmail.com>
2022-09-22 22:27:16 +0530

    feat(theme): add custom theme

--- rc.lua
+++ rc.lua
@@ -10,6 +10,8 @@ require 'wallpaper'
 local awful = require 'awful'
 local naughty = require 'naughty'
 local ruled = require 'ruled'
+local beautiful = require 'beautiful'
+local gears = require 'gears'
 
 local maps = require 'maps'
 local widgets = require 'widgets'
@@ -20,6 +22,9 @@ awful.spawn.once 'xrandr --output HDMI-2 --mode 1920x1080 --rate 60'
 awful.spawn.once 'xset r rate 500 60'
 awful.spawn.once 'picom --experimental-backend'
 awful.spawn.once 'lxqt-policykit-agent'
+
+-- Theme
+beautiful.init(gears.filesystem.get_configuration_dir() .. 'theme.lua')
 
 tag.connect_signal('request::default_layouts', function()
   awful.layout.append_default_layouts {
---
+++ theme.lua
@@ -0,0 +1,13 @@
+local bg = '#172026'
+local bg_lighter = '#027373'
+local fg = '#5FCDD9'
+local alpha = '2F'
+
+local theme = {}
+
+theme.font = 'IosevkaSCJ Nerd Font 20'
+theme.bg_normal = bg .. alpha
+theme.bg_focus = bg_lighter .. alpha
+theme.fg_normal = fg
+
+return theme
