1bf1430a55c743178a325d7247525f9047228ddc
Sachin Charakhwal <sachincharakhwal@gmail.com>
2022-09-26 17:46:50 +0530

    feat: remove floating layout

--- rc.lua
+++ rc.lua
@@ -31,7 +31,6 @@ awful.spawn.once 'copyq'
 tag.connect_signal('request::default_layouts', function()
   awful.layout.append_default_layouts {
     awful.layout.suit.tile,
-    awful.layout.suit.floating,
   }
 end)
 
